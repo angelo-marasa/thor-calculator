610ee4244c1e9892d6e643d3b58516a4c92b0968
angelo-marasa <angelo.marasa@kaleidico.com>
2022-02-16 11:21:09 -0500

    Fixed a tailwind padding issue on container

--- src/App.js
+++ src/App.js
@@ -71,7 +71,7 @@ const App = () => {
   return (
     <>
     <div className="bg-gradient-to-r from-gray-900 to-gray-600 bg-gradient-to-r h-screen">
-            <div className="container mx-auto pt-6 text-center">
+            <div className="container px-4 mx-auto pt-6 text-center">
                 <h1 className="text-3xl font-bold text-white">
                     Thor Nodes Calculator
                 </h1>
@@ -116,7 +116,7 @@ const App = () => {
                         onInput={ ( e ) => handleOdinNodes( e.target.value ) }/>
                     </div>
                 </div>
-                <p className="text-white pt-10 text-left">You're currently earning <span className="rounded-r-lg rounded-l-lg bg-amber-400 p-2 ml-2">{dailyRewards.toFixed(3)}</span> Thor per 24 hours. This is equal to <span className="rounded-r-lg rounded-l-lg bg-amber-400 p-2 ml-2">${(dailyRewards * thorPrice).toFixed(2)}</span> USD per day.</p>
+                <p className="text-white pt-10 text-left leading-10">You're currently earning <span className="rounded-r-lg rounded-l-lg bg-amber-400 p-2 ml-2">{dailyRewards.toFixed(3)}</span> Thor per 24 hours. This is equal to <span className="rounded-r-lg rounded-l-lg bg-amber-400 p-2 ml-2">${(dailyRewards * thorPrice).toFixed(2)}</span> USD per day.</p>
                 <hr className="mt-10 mb-10"/>
                 <p className="text-white">This calculator gives you an idea of what it would look like for you to either taking some passive income or compounding. Above, you see what you're making per day. Let's select a larger time frame and see what you can do.</p>
             </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,14 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    container: (theme) => ({
+      padding: {
+        default: theme("spacing.4"),
+        sm: theme("spacing.5"),
+        lg: theme("spacing.6"),
+        xl: theme("spacing.8"),
+      },
+    }),
   },
   plugins: [],
 }
