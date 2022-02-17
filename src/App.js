4a89472ec45f81c950dbd861555b57e07952e772
angelo-marasa <angelo.marasa@kaleidico.com>
2022-02-17 15:55:14 -0500

    Added cleaner styling

--- src/App.js
+++ src/App.js
@@ -23,6 +23,16 @@ const App = () => {
   const thorRewards = .144
   const odinRewards = 1.02;
 
+  const heimdalCost = 1.25;
+  const freyaCost = 6.25;
+  const thorCost = 12.5;
+  const odinCost = 78.25;
+
+  const heimdalTax = 1;
+  const freyaTax = 5;
+  const thorTax = 8;
+  const odinTax = 10;
+
   //-- Setter functions ------------------------
   function handleThorPrice(price) {
     setThorPrice(price);
@@ -137,7 +147,7 @@ const App = () => {
                 
                 <div className="mt-20">
                 <fieldset className="space-y-6">
-                <div className="grid sm:grid-cols-4 gap-6">
+                <div className="grid sm:grid-cols-6 gap-6">
                 <label for="one-day" className="relative flex flex-col bg-white p-5 rounded-lg shadow-md cursor-pointer">
                     <span className="font-semibold text-gray-500 leading-tight uppercase mb-3 text-center">1 Day</span>
                     <input type="radio" name="plan" id="one-day" value="1" className="absolute h-0 w-0 appearance-none" onChange={ ( e ) => handleActionDays( e.target.value ) }/>
@@ -183,47 +193,113 @@ const App = () => {
                         </span>
                     </span>
                 </label>
+                <label for="one-eighty" className="relative flex flex-col bg-white p-5 rounded-lg shadow-md cursor-pointer">
+                    <span className="font-semibold text-gray-500 leading-tight uppercase mb-3 text-center">180 Days</span>
+                    <input type="radio" name="plan" id="one-eighty" value="180" className="absolute h-0 w-0 appearance-none"  onChange={ ( e ) => handleActionDays( e.target.value ) }/>
+                    <span aria-hidden="true" className="hidden absolute inset-0 border-2 border-amber-500 bg-amber-200 bg-opacity-10 rounded-lg">
+                        <span className="absolute top-4 right-4 h-6 w-6 inline-flex items-center justify-center rounded-full bg-amber-200">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="h-5 w-5 text-amber-600">
+                            <path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd" />
+                        </svg>
+                        </span>
+                    </span>
+                </label>
+                <label for="three-sixty-five" className="relative flex flex-col bg-white p-5 rounded-lg shadow-md cursor-pointer">
+                    <span className="font-semibold text-gray-500 leading-tight uppercase mb-3 text-center">365 Days</span>
+                    <input type="radio" name="plan" id="three-sixty-five" value="365" className="absolute h-0 w-0 appearance-none"  onChange={ ( e ) => handleActionDays( e.target.value ) }/>
+                    <span aria-hidden="true" className="hidden absolute inset-0 border-2 border-amber-500 bg-amber-200 bg-opacity-10 rounded-lg">
+                        <span className="absolute top-4 right-4 h-6 w-6 inline-flex items-center justify-center rounded-full bg-amber-200">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="h-5 w-5 text-amber-600">
+                            <path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd" />
+                        </svg>
+                        </span>
+                    </span>
+                </label>
             </div>
             </fieldset>
             
                 </div>
 
-            { ((actionDays > 0) && (heimdallNode > 0 || freyaNode > 0 || thorNode > 0 || odinNode > 0)) && 
+            {/* { ((actionDays > 0) && (heimdallNode > 0 || freyaNode > 0 || thorNode > 0 || odinNode > 0)) && 
                 <p className="text-white text-left mt-5">In {actionDays} days, you'll have: </p>
-            }
-            
+            } */}
+            <div class="grid grid-cols-4 gap-4">
             { heimdallNode > 0 && 
-                <div className="heimdall-node mt-5">
-                    <p className="text-white text-left">{((heimdallNode * hemidalRewards) * actionDays).toFixed(3)} Thor from your Heimdall Nodes</p> 
-                    <p className="text-white text-left">If you were looking to take profits, you're value after Claim Tax is { ((((heimdallNode * hemidalRewards) * actionDays)) - (((heimdallNode * hemidalRewards) * actionDays) * (1 / 100))).toFixed(4) }</p> 
-                
+                <div className="mt-10">
+                    <div className="p-6 max-w-sm bg-gray-800 rounded-lg border border-gray-700 shadow-md hover:bg-gray-700">
+                        <h5 className="mb-2 text-2xl font-bold tracking-tight text-white">Your Heimdall Nodes</h5>
+                        <p className="font-normal text-gray-400">{((heimdallNode * hemidalRewards) * actionDays).toFixed(3)} Thor Rewards / ${ (((heimdallNode * hemidalRewards)* thorPrice) - (((heimdallNode * hemidalRewards)* thorPrice) * heimdalTax / 100)).toFixed(2) } after Claim Tax.</p>
+                            {
+                                ((heimdalCost / (heimdallNode * hemidalRewards))  > actionDays) && 
+                                <p className="font-normal text-gray-700 mt-2">You need an additional { (heimdalCost/ (heimdallNode * hemidalRewards) - actionDays).toFixed(2)} days to compound into another Heimdall Node.</p>
+                                 
+                            }    
+                        
+                            {
+                                ((heimdalCost / (heimdallNode * hemidalRewards)) <= actionDays ) && 
+                                <p className="font-normal text-amber-200 mt-2">You have enough Thor from this node to compound into another Heimdall Node.</p>
+                            }
+                    </div>
                 </div>
             }
 
             { freyaNode > 0 && 
-                <div className="freya-node mt-5">
-                    <p className="text-white text-left">{((freyaNode * freyaRewards) * actionDays).toFixed(3)} Thor from your Freya Nodes</p> 
-                    <p className="text-white text-left">If you were looking to take profits, you're value after Claim Tax is { ((((freyaNode * freyaRewards) * actionDays)) - (((freyaNode * freyaRewards) * actionDays) * (5 / 100))).toFixed(4) }</p> 
-
+                <div className="mt-10">
+                    <div className="p-6 max-w-sm bg-gray-800 rounded-lg border border-gray-700 shadow-md hover:bg-gray-700">
+                        <h5 className="mb-2 text-2xl font-bold tracking-tight text-white">Your Freya Nodes</h5>
+                        <p className="font-normal text-gray-400">{((freyaNode * freyaRewards) * actionDays).toFixed(3)} Thor Rewards / ${ (((freyaNode * freyaRewards)* thorPrice) - (((freyaNode * freyaRewards)* thorPrice) * freyaTax / 100)).toFixed(2) } after Claim Tax.</p>
+                            {
+                                ((freyaCost/ (freyaNode * freyaRewards))  > actionDays) && 
+                                <p className="font-normal text-gray-700 dark:text-gray-400 mt-2">You need an additional { (freyaCost / (freyaNode * freyaRewards) - actionDays).toFixed(2)} days to compound into another Freya Node.</p>
+                                
+                            }    
+                        
+                            {
+                                ((freyaCost/ (freyaNode * freyaRewards)) <= actionDays ) && 
+                                <p className="font-normal text-amber-200 mt-2">You have enough Thor from this node to compound into another Freya Node.</p>
+                            }
+                    </div>
                 </div>
             }
             
             { thorNode > 0 && 
-                <div className="thor-node mt-5">
-                    <p className="text-white text-left">{((thorNode * thorRewards) * actionDays).toFixed(3)} Thor from your Thor Nodes</p> 
-                    <p className="text-white text-left">If you were looking to take profits, you're value after Claim Tax is { ((((thorNode * thorRewards) * actionDays)) - (((thorNode * thorRewards) * actionDays) * (8 / 100))).toFixed(4) }</p> 
-
+                <div className="mt-10">
+                    <div className="p-6 max-w-sm bg-gray-800 rounded-lg border border-gray-700 shadow-md hover:bg-gray-700">
+                        <h5 className="mb-2 text-2xl font-bold tracking-tight text-white">Your Thor Nodes</h5>
+                        <p className="font-normal text-gray-400">{((thorNode * thorRewards) * actionDays).toFixed(3)} Thor Rewards / ${ (((thorNode * thorRewards)* thorPrice) - (((thorNode * thorRewards)* thorPrice) * thorTax / 100)).toFixed(2) } after Claim Tax.</p>
+                            {
+                                ((thorCost / (thorNode * thorRewards))  > actionDays) && 
+                                <p className="font-normal text-gray-700 dark:text-gray-400 mt-2">You need an additional { (thorCost / (thorNode * thorRewards) - actionDays).toFixed(2)} days to compound into another Thor Node.</p>
+                                
+                            }    
+                        
+                            {
+                                ((thorCost / (thorNode * thorRewards)) <= actionDays ) && 
+                                <p className="font-normal text-amber-200 mt-2">You have enough Thor from this node to compound into another Thor Node.</p>
+                            }
+                    </div>
                 </div>
             }
 
             { odinNode > 0 && 
-                <div className="odin-node mt-5"> 
-                    <p className="text-white text-left">{((odinNode * odinRewards) * actionDays).toFixed(3)} Thor from your Odin Nodes</p>
-                    <p className="text-white text-left">If you were looking to take profits, you're value after Claim Tax is { ((((odinNode * odinRewards) * actionDays)) - (((odinNode * odinRewards) * actionDays) * (10/ 100))).toFixed(4) }</p> 
-
-                </div> 
+                <div className="mt-10">
+                    <div className="p-6 max-w-sm bg-gray-800 rounded-lg border border-gray-700 shadow-md hover:bg-gray-700">
+                        <h5 className="mb-2 text-2xl font-bold tracking-tight text-white">Your Odin Nodes</h5>
+                        <p className="font-normal text-gray-400">{((odinNode * odinRewards) * actionDays).toFixed(3)} Thor Rewards / ${ (((odinNode * odinRewards)* thorPrice) - (((odinNode * odinRewards)* thorPrice) * odinTax / 100)).toFixed(2) } after Claim Tax.</p>
+                            {
+                                ((odinCost / (odinNode * odinRewards))  > actionDays) && 
+                                <p className="font-normal text-gray-700 dark:text-gray-400 mt-2">You need an additional { (odinCost / (odinNode * odinRewards) - actionDays).toFixed(2)} days to compound into another Odin Node.</p>
+                                
+                            }    
+                        
+                            {
+                                ((odinCost / (odinNode * odinRewards)) <= actionDays ) && 
+                                <p className="font-normal text-amber-200 mt-2">You have enough Thor from this node to compound into another Odin Node.</p>
+                            }
+                    </div>
+                </div>
             }
-
+            </div>
             <h3 className="text-lg mt-20 text-gray-400">[RoT, Compounding and reward taking still in the works]</h3>
             
             
